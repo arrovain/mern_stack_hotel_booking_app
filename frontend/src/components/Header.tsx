f376c27d522cf086644a55b436a26d73796d3661
icme <icmemahmut@gmail.com>
2024-06-28 16:31:09 +0300

    sign in link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,12 @@ const Header = () => {
         <div className="bg-blue-800 py-6">
             <div className="container mx-auto flex justify-between">
                 <span className="text-3xl text-white font-bold tracking-tight">
-                    <Link>
+                    <Link to="/"> MernHolidays.com
                     </Link>
+                    <span className="flex space-x-2">
+                        <Link className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100" to="/sign-in">
+                        </Link>
+                    </span>
                 </span>
             </div>
         </div>
