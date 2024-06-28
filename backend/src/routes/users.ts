5bcb3c9ef871dc76e40daf88de5bd7540fc1dea6
icme <icmemahmut@gmail.com>
2024-06-28 17:55:08 +0300

    cookie

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -16,7 +16,14 @@ router.post("/register", async (req: Request, res:Response)=>{
         await user.save();
         const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET_KEY as string, {
             expiresIn:"1d"
-        } )
+        } );
+
+        res.cookie("auth_token", token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 86400000
+       })
+       return res.sendStatus(200);
     }
     catch(error){
         res.status(500).send({"something went wrong"})
